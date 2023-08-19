fc4be2ac7e0824a57c609fb6fe1fbff0bf4873bf
techkah <kassandraferreira01@gmail.com>
2023-08-19 15:52:44 -0300

    atualizaçõesz no css

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ app= Flask(__name__)
 def index():
     if request.method == 'POST':
         nome= request.form['nome']
+        sobrenome= request.form['sobrenome']
         email= request.form['email']
-        number= request.form['number']
-        estudo= request.form['estudo']
-        trabalho= request.form['trabalho']
-        curiosidade= request.form['curiosidade']
-        colaborador= request.form['colaborador']
-        outros= request.form['outros']
-        permissao= request.form['permissao']
-        return f"nome: {nome} <br> email: {email} <br> number: {number} <br> estudo: {estudo} <br> trabalho: {trabalho} <br> curiosidade: {curiosidade} <br> colaborador: {colaborador} <br> outros: {outros} <br> permissao: {permissao}"
+        telefone= request.form['telefone']
+        estudo= request.form.get('estudo')
+        trabalho= request.form.get('trabalho')
+        curiosidade= request.form.get('curiosidade')
+        conhecimento= request.form.get('conhecimento')
+        permissao= request.form.get('permissao')
+        negado= request.form.get('negado')
+        return f"nome: {nome} <br> sobrenome: {sobrenome} <br> email: {email} <br> telefone: {telefone} <br> estudo: {estudo} <br> trabalho: {trabalho} <br> curiosidade: {curiosidade} <br> conhecimento: {conhecimento} <br> permissao: {permissao} <br> negado: {negado} "
+    
     return render_template ('index.html')
 
 if __name__ == '_main_':
